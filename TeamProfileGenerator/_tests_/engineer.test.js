4203184bcf10cc28c239fb034691fdeb904a311e
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-17 15:37:35 -0700

    engineer test added

---
+++ TeamProfileGenerator/_tests_/engineer.test.js
@@ -0,0 +1,19 @@
+const engineer = require("../lib/engineer");
+
+test("GitHub constructor", () => {
+    const testValue = "maxlin-95";
+    const employee = new engineer("Max", 1, "max@test.com", testValue);
+    expect(employee.github).toBe(testValue);
+})
+
+test("getRole function", () => {
+    const testValue = "engineer";
+    const employee = new engineer("Max", 1, "max@test.com", testValue);
+    expect(employee.getRole()).toBe(testValue);
+})
+
+test("Get the Github account", () => {
+    const testValue = "maxlin-95";
+    const employee = new engineer("Max", 1, "max@test.com", testValue);
+    expect(employee.getGithub()).toBe(testValue);
+})
